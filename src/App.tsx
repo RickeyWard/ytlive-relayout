50e57655aad4dcf8283ae347d755ed78c4de335b
DiamondDrake <RickeyWard@users.noreply.github.com>
2023-06-29 23:02:22 -0400

    add youtube controls back in

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
       {vId ? <>
       <div className="w-100 h-100 flex-grow">
         <iframe width="100%" height="100%"
-          src={`https://www.youtube.com/embed/${vId}?controls=0&showinfo=0&autoplay=1`}
+          src={`https://www.youtube.com/embed/${vId}?controls=1&showinfo=0&autoplay=1`}
           title="YouTube video player" frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
