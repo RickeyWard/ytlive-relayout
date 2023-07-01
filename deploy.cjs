824fe96903a3e6b3bde342384649854bee69f3e2
DiamondDrake <RickeyWard@users.noreply.github.com>
2023-06-30 22:27:44 -0400

    Update deploy.cjs

--- deploy.cjs
+++ deploy.cjs
@@ -1,8 +1,6 @@
 var FtpDeploy = require("ftp-deploy");
 var ftpDeploy = new FtpDeploy();
 
-console.log("testing: " + process.env.FTPPASSWORD);
-
 var config = {
     user: "githubdeploy",
     host: "www.diamonddrake.com",
